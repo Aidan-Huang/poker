b0869c317e1c297741d82c69d157b47de73cacf5
Aidan <hyideal@gmail.com>
2022-03-16 21:38:42 +0800

    add unittest

--- poker.py
+++ poker.py
@@ -5,6 +5,13 @@ class Poker():
     def __str__(self) -> str:
         return f"Poker: {self.cards_num} cards"
 
+    def is_sister(self, cards):
+        if len(cards) < 4:
+            return False
+        elif len(cards) % 2 != 0:
+            return False
+        return True
+
 
 
 poker = Poker()
