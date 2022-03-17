9019cd6a4fa5299416e30b573165dffda99d3009
Aidan <hyideal@gmail.com>
2022-03-17 13:42:20 +0800

    增加扑克单例模式
    构造扑克牌堆

--- poker.py
+++ poker.py
@@ -1,6 +1,48 @@
-class Poker():
+class single:
+    card = -1
+
+
+class double:
+    cards = [-1, -1]
+
+
+class trible:
+    cards = [-1, -1, -1]
+
+
+class sister:
+    cards = []
+
+
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+"""
+定义扑克类
+"""
+
+
+class Poker(metaclass=Singleton):
+    BLACK_JOKER = 50
+    RED_JOKER = 100
 
     cards_num = 54
+    deck = []
+
+    def __init__(self):
+
+        for suit in range(4):
+            for i in range(13):
+                self.deck.append(i + 1)
+
+        self.deck.append(Poker.BLACK_JOKER)
+        self.deck.append(Poker.RED_JOKER)
 
     def __str__(self) -> str:
         return f"Poker: {self.cards_num} cards"
@@ -11,9 +53,3 @@ class Poker():
         elif len(cards) % 2 != 0:
             return False
         return True
-
-
-
-poker = Poker()
-
-print(poker)
--- testPoker.py
+++ testPoker.py
@@ -1,27 +1,35 @@
 import unittest
 from poker import Poker
 
+
 class TestStringMethods(unittest.TestCase):
+
+    poker = Poker()
+
+    def test_init(self):
+        self.assertEqual(54, len(self.poker.deck))
+        print(self.poker.deck)
+
+    def test_str(self):
+        self.assertEqual("Poker: 54 cards", str(self.poker))
 
     def test_is_sister(self):
 
-        poker = Poker()
-
         # 小于4张牌
         cards = [7, 7]
-        self.assertFalse(poker.is_sister(cards))
+        self.assertFalse(self.poker.is_sister(cards))
         cards = [7, 7, 8]
-        self.assertFalse(poker.is_sister(cards))
+        self.assertFalse(self.poker.is_sister(cards))
 
         # 单数牌
         cards = [7, 7, 8, 8, 9]
-        self.assertFalse(poker.is_sister(cards))
+        self.assertFalse(self.poker.is_sister(cards))
 
         # 2姐妹 顺序排列
         cards = [7, 7, 8, 8]
-        self.assertTrue(poker.is_sister(cards))
+        self.assertTrue(self.poker.is_sister(cards))
         cards = [8, 8, 7, 7]
-        self.assertTrue(poker.is_sister(cards))
+        self.assertTrue(self.poker.is_sister(cards))
 
         # # 4
         # cards = [7, 8, 7, 9]
