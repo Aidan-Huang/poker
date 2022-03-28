341114a15f2e30001d6db7b90d54177a0a98b162
Aidan <hyideal@gmail.com>
2022-03-28 16:02:39 +0800

    输入错误处理，增加显示对方剩余牌数

--- main.py
+++ main.py
@@ -26,15 +26,19 @@ eric.hand_cards.sort()
 
 # 确认出牌玩家
 current_player = Poker.get_first_player()
+other_cards_number = 18
 current_type = None
 current_type_player = None
 has_cards_to_play = "yes"
+valid_input = False
 
 def info_to_chang_player():
     global current_player
-    # input("Ready to change to next player.")
-    # os.system('clear')
-    # input("Please let the screen to the next player ")
+    global other_cards_number
+    input("Ready to change to next player.")
+    os.system('clear')
+    input("Please let the screen to the next player ")
+    other_cards_number = len(current_player.hand_cards)
     current_player = Poker.get_next_human()
     # print(f"after change, current player is:{current_player.name}")
 
@@ -45,14 +49,20 @@ while poker.get_winner() is None:
     print(current_player.hand_cards)
 
     if current_type is not None and current_player != current_type_player:
-        print(f"current cards: {current_type}")
+        print(f"current cards: {current_type}, other's hand cards remain {other_cards_number}")
         # has_cards_to_play = input("Do you want to play cards against current?(Yes, No):").lower()
         # if has_cards_to_play == "no":
         #     current_type = None
         #     info_to_chang_player()
         #     continue
+    valid_input = False
+    while not valid_input:
+        try:
+            cards = [int(x) for x in input("Please input cards you want to play:").split()]
+            valid_input = True
+        except Exception:
+            print("Input error, input cards with a blank like '2 3 4'")
 
-    cards = [int(x) for x in input("Please input cards you want to play:").split()]
     card_type = Poker.analyze_card(cards)
     # 出牌是否合法
     # 如果出牌，牌型有效并且手牌包含所有组成牌型牌
@@ -60,7 +70,16 @@ while poker.get_winner() is None:
     if current_type is None:
         while card_type.name == CardType.CARD_TYPE_INVALID_TYPE or not Common.is_contain_sub_list(
                 current_player.hand_cards, cards):
-            cards = [int(x) for x in input("The cards your played is illegal, please try again:").split()]
+            print(current_player.hand_cards)
+            valid_input = False
+            while not valid_input:
+                try:
+                    cards = [int(x) for x in input("The cards your played is illegal, please try again:").split()]
+                    valid_input = True
+                except Exception:
+                    print("Input error, input cards with a blank like '2 3 4'")
+
+
             card_type = Poker.analyze_card(cards)
     else:
         while not card_type > current_type or not Common.is_contain_sub_list(
@@ -72,7 +91,15 @@ while poker.get_winner() is None:
                 info_to_chang_player()
                 break
             else:
-                cards = [int(x) for x in input("Please input cards you want to play:").split()]
+                print(current_player.hand_cards)
+                print(f"current cards: {current_type}, other's hand cards remain {other_cards_number}")
+                valid_input = False
+                while not valid_input:
+                    try:
+                        cards = [int(x) for x in input("Please input cards you want to play:").split()]
+                        valid_input = True
+                    except Exception:
+                        print("Input error, input cards with a blank like '2 3 4'")
                 card_type = Poker.analyze_card(cards)
 
     if has_cards_to_play == "no":
